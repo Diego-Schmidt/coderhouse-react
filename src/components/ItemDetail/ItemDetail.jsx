095913b61d4b162201eee343177313a862a2295f
Diego-Schmidt <email@diegoschmidt.com>
2022-05-03 08:32:04 -0300

    Agregados estilos bootstrap para ItemDetal.jsx , detalle de producto

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,5 +1,6 @@
-import { Card, Badge } from 'react-bootstrap';
+import { Badge } from 'react-bootstrap';
 import ItemCount from '../ItemCount/ItemCount';
+// import css from './ItemDetail.css';
 
 
 
@@ -8,19 +9,22 @@ import ItemCount from '../ItemCount/ItemCount';
 
 function ItemDetail({  detalle } ) {
   return (
-    <Card className="bg-warning shadow-lg p-3 mb-3 mr-2 ml-2 rounded">
-    <Card.Title>Detalles de {detalle.name}</Card.Title>
-    <Card.Img variant="top" src={detalle.picture} />
-    <Card.Body>
-    <Card.Text>
-        {detalle.description}
-    </Card.Text>
-    <Badge bg="success">{detalle.price} $</Badge>
+    <div className="container col-xxl-8 px-4 py-5">
+    <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
+      <div className="col-10 col-sm-8 col-lg-6">
+        <img src={detalle.picture} className="d-block mx-lg-auto img-fluid" alt={detalle.name} width="700" height="500" loading="lazy" />
+      </div>
+      <div className="col-lg-6">
+        <h1 className="display-5 fw-bold lh-1 mb-3">{detalle.name}</h1>
+        <p className="lead">{detalle.description}</p>
+        
+        <Badge bg="success">{detalle.price} $</Badge>
     <Badge>Stock {detalle.stock}</Badge>
-    </Card.Body>
-    <ItemCount stock={detalle.stock} initial={1} itemName={detalle.name} />
-  </Card>
-   
+          <ItemCount stock={detalle.stock} initial={1} itemName={detalle.name} />
+        
+      </div>
+    </div>
+  </div>
   )
 }
 
