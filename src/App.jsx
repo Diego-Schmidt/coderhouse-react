16f218909daf4f7ab9abf97248d92156fcf4f108
Diego-Schmidt <email@diegoschmidt.com>
2022-05-13 00:35:21 -0300

    cambio de basename a /coderhouse-react/#

--- src/App.jsx
+++ src/App.jsx
@@ -12,14 +12,14 @@ function App() {
   return (
     <>
     <CartContextProvider>
-    <BrowserRouter basename="/coderhouse-react">
+    <BrowserRouter basename="/coderhouse-react/#">
       <NavBar />
       <main>
       <Routes>
         <Route path="/item/:itemid" element={<ItemDetailContainer greeting="Detalle de producto"/>} />  
         <Route path="/" element={<ItemListContainer greeting="Menú de Habemus Papa!"/>} />
         <Route path="/cart" element={<Cart greeting="Este es su carrito"/>} / >
-        <Route path="/coderhouse-react" exact component={<ItemListContainer greeting="Bienvenido a Habemus Papa!"/>} />
+        {/* <Route path="/coderhouse-react" exact component={<ItemListContainer greeting="Bienvenido a Habemus Papa!"/>} /> */}
         <Route path="/category/:categoryid" element={<ItemListContainer greeting="Categoría"/>} />
       </Routes>
       <hr />
