dfa1748e6d6a3aa39ca6041129acf807fb83c315
Diego-Schmidt <email@diegoschmidt.com>
2022-05-13 00:56:50 -0300

    Prueba 5 con HashRouter para solucionar compatibilidad con GH pages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 // import ReactDOM from 'react-dom';
 import {createRoot} from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -11,7 +12,9 @@ const root = createRoot(rootElement);
 // const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <HashRouter>
     <App />
+    </HashRouter>
   </React.StrictMode>
 );
 
